f87ccd58478612945fa41d444a16249ce68b45e1
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-11-27 09:59:02 +0530

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -2,7 +2,7 @@
 
 def sample_responses(input_text):
     user_message = str(input_text).lower()
-
+                                                                                                                        
     if user_message in ("hello", "hi", "sup","hiii","hey"):
         return "Hey! Welcome to Gogte Institute of Technology"
     if user_message in ("who are you", "who are you?"):
